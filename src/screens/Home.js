f3f5a1499ffa7a76a8dc8c9f00310aaae404cec4
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-01 13:15:58 +0530

    icons-added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Icon from "react-native-vector-icons/Entypo";
+
 export default function Home() {
     return (
         <View>
+            <Icon name="arrow-long-left" size={30} color="#900" />
             <Text>Home</Text>
         </View>
     )
